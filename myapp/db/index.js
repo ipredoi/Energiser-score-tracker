8a0fa2dbdf21e66090a8ddc0d830bedcb96fa0d2
Ionut Predoi <ipredoi27@gmail.com>
2021-02-04 20:12:49 +0000

    pg pool connection updated

--- myapp/db/index.js
+++ myapp/db/index.js
@@ -1,12 +1,13 @@
-const {Pool} = require('pg');
+const { Pool } = require("pg");
 const pool = new Pool({
-    ssl: {
-        rejectUnauthorized: false,
-      }
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 module.exports = {
-    query: (text, params, callback) => {
-     return pool.query(text, params, callback);
-    },
-  };
+	query: (text, params, callback) => {
+		return pool.query(text, params, callback);
+	},
+};
